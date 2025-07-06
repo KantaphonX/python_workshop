c66eb38a727a2e686bb15e8024c93a0c097b8b48
KantaphonX <guide1300@gmail.com>
2025-07-06 11:53:16 +0700

    V2 python02-07

---
+++ python02.py
@@ -0,0 +1,25 @@
+# ตัวแปร
+fullname = "น้ำหวาน" #ข้อมูล string
+age = 25 #ข้อมูล number
+major = 'IoT' #ข้อมูล string
+stuStatus = True #หรือ False รเียกว่าเป็นข้อมูล boolean
+detail = '''
+            หล่อ
+            รวย
+            เก่ง
+            เท่ห์
+         ''' #ข้อมูล string
+foodLove = """
+            KFC
+                 Pizza
+  Expresso
+           """ #ข้อมูล string
+ 
+total = 10 + 30 + 40  #expression นิพจน์ คือ 10 + 30 + 40 คือ อะไรที่มีโพรเซสการทำงาน
+ 
+print(fullname)
+print(age)
+print(major)
+print(stuStatus)
+print(detail)
+print(foodLove)
